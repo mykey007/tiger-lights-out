b99e9e63bfe0ceff44c3ac05c7070ffb95be19df
Michael Mallowe <mmallowe@gmail.com>
2020-04-23 17:41:43 -0700

    making board a table that takes a cell component

--- src/Board.js
+++ src/Board.js
@@ -2,13 +2,71 @@ import React, { Component } from 'react'
 import "./Board.css"
 import Cell from "./Cell"
 
+/** Game board of Lights out.
+ *
+ * Properties:
+ *
+ * - nrows: number of rows of board
+ * - ncols: number of cols of board
+ * - chanceLightStartsOn: float, chance any cell is lit at start of game
+ *
+ * State:
+ *
+ * - hasWon: boolean, true when board is all off
+ * - board: array-of-arrays of true/false
+ *
+ *    For this board:
+ *       .  .  .
+ *       O  O  .     (where . is off, and O is on)
+ *       .  .  .
+ *
+ *    This would be: [[f, f, f], [t, t, f], [f, f, f]]
+ *
+ *  This should render an HTML table of individual <Cell /> components.
+ *
+ *  This doesn't handle any clicks --- clicks are on individual cells
+ *
+ **/
 
 class Board extends Component {
+
+    constructor(props) {
+        super(props)
+
+        // TODO: set initial state
+        this.setState ={ nrows: 5, ncols: 5}
+    }
+
+    createBoard() {
+        let board = []
+        // create array of arrays of true/false values
+        return board
+    }
+
+
+    flipCellsAround(coord) {
+        let { ncols, nrows } = this.props
+        let board = this.state.board
+        let [x, y] = coord.split('-').map(Number)
+
+        function flipCell(y, x) {
+            // if this coord is on board, flip it
+            if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
+                board[y][x] = !board[y][x]
+            }
+        }
+        this.setState({board, hasWon})
+    }
+
     render() {
         return(
-            <div className='App'>
-                <Cell />
-            </div>
+            <table className='Board'>
+                <tbody>
+                    <tr>
+                        <Cell/>
+                    </tr>
+                </tbody>
+            </table>
         )   
     }
 }
--- src/Cell.js
+++ src/Cell.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
 import "./Cell.css"
 
-
 class Cell extends Component {
+    constructor(props) {
+        super(props)
+        this.handleClick = this.handleClick.bind(this)
+        this.setState={}
+    }
+
+    handleClick(evt) {
+        //call up to the board to flip cells around this cell
+        this.props.flipCellsAroundMe()
+    }
+
     render() {
-        return(
-            <div className='Cell'>
-                {/* something */}
-            </div>
+        let classes = 'Cell' + (this.props.isLit ? " Cell-lit" : "")
+        return( 
+            <td className={classes} onClick={this.handleClick}/>
         )   
     }
 }
